a1ac22784fe18b7df32d3a93071e3d5b40d6c095
yvMuhammed <jamiu@youverify.co>
2022-03-04 13:11:55 +0100

    ipdate

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,5 @@
 export default {
-	mode: 'universal',
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: "youstore-frontend",
@@ -15,7 +15,7 @@ export default {
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
   //Customize the progress-bar color
-  loading: {color:'#46B2C8', height:'4px', duration: 5000},
+  loading: {color:'#46B2C8', height:'4px',},
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ["@/assets/css/tailwind.css"],
