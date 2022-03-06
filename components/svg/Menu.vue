275ab318c5c41d6f71e8234c170bdd1ae337727e
chimailo <collins.chima.ilo@gmail.com>
2022-03-06 07:19:04 +0100

    create menu and cancel svg components

---
+++ components/svg/Menu.vue
@@ -0,0 +1,16 @@
+<template>
+  <base-svg viewBox="0 0 24 24" size="small">
+  <path fill="currentColor" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
+  </base-svg>
+</template>
+
+<script>
+import BaseSvg from "./Base.vue";
+
+export default {
+  name: "MenuIcon",
+  components: {
+    "base-svg": BaseSvg,
+  },
+};
+</script>
