e64ea490dfc3bf851a60a46a5a619bfd47897e7c
chimailo <collins.chima.ilo@gmail.com>
2022-03-18 09:15:37 +0100

    auth-merchant middleware

--- components/Modals/index.vue
+++ components/Modals/index.vue
@@ -1,26 +1,22 @@
 <template>
-  <div class="modal-backdrop fixed top-0 bottom-0 left-0 right-0 flex justify-center items-center bg-black/30">
-    <div class="modal bg-white shadow-md overflow-x-auto flex flex-col">
-      <header class="modal-header relative justify-between p-4 flex border-b border-b-secondary-400">
+  <div role="presentation" class="modal-root fixed z-50 inset-0 ">
+  <!-- <div class="modal-backdrop fixed top-0 bottom-0 left-0 right-0 flex justify-center items-center bg-black/30"> -->
+    <slot name="backdrop"></slot>
+    <div role="presentation" tabindex="-1" class="modal-container h-full justify-center items-center flex">
+      <div role="dialog" aria-labelledby="modal-title" aria-describedby="modal-content" class="modal text-secondary-800 rounded transition relative overflow-y-auto m-8 bg-white shadow-md max-w-[600px] max-h-[calc(100%_-_64px)] flex-col flex">
+      <div id="modal-title" class="modal-header py-4 px-6 flex-[0_0_auto] flex">
         <slot name="header"></slot>
-        <icon-button
-          class="!absolute top-0 right-0"
-          @click="close"
-        >
-          x
-        </icon-button>
-      </header>
+      </div>
+      
+      <div id="modal-content" class="modal-content relative flex-auto overflow-y-auto pt-0 py-5 px-6 border-b border-b-secondary-400 border-t border-t-secondary-400">
+        <slot name="content"></slot>
+      </div>
 
-      <section class="modal-body relative pt-5 px-3">
-        <slot name="body">
-          This is the default body!
-        </slot>
-       </section>
-
-      <footer class="modal-footer relative justify-between p-4 flex border-b border-b-secondary-400">
-        <slot name="footer"></slot>
-      </footer>
+      <div class="modal-actions flex items-center justify-end flex-[0_0_auto] p-2 relative">
+        <slot name="actions"></slot>
+      </div>
     </div>
+  </div>
   </div>
 </template>
 
@@ -36,5 +32,4 @@ export default {
 </script>
 
 <style>
-
 </style>
