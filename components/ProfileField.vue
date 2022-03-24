7583ef0afcb44eed393d98b00e4513cbc25ef2ff
chimailo <collins.chima.ilo@gmail.com>
2022-03-24 22:41:30 +0100

    svg components

---
+++ components/ProfileField.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="flex">
+        <div v-if="editing" class="flex-grow">
+            <label :for="label" class="font-medium text-xl mb-4">{{ label }}</label>
+            <input :id="label" v-model="input" :type="type" class="after:w-full after:h-[1px] after:bg-secondary-200 my-1">
+            <p class="after:w-full after:h-[1px] after:bg-secondary-200 my-1">{{ value }}</p>
+        </div>
+        <div v-else class="flex-grow">
+            <h4 class="font-medium mb-4 text-xl">{{ label }}</h4>
+            <p class="after:w-full after:h-[1px] after:bg-secondary-200 my-1">{{ value }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        editing: {
+            type: Boolean,
+            default: false
+        },
+        label: {
+            type: String,
+            default: ''
+        },
+        type: {
+            type: String,
+            default: ''
+        },
+        value: {
+            type: String,
+            default: ''
+        }
+    },
+    data() {
+        return {
+            input: value
+        }
+    }
+}
+</script>
---
+++ components/svg/Withdrawal.vue
@@ -0,0 +1,11 @@
+<template>
+  <base-svg viewBox="0 0 20 16" color="inherit">
+    <path d="M13 10V7H16V5L20 8.5L16 12V10H13ZM12 3.7V5H0V3.7L6 0L12 3.7ZM5 6H7V11H5V6ZM1 6H3V11H1V6ZM11 6V8.5L9 10.3V6H11ZM7.1 12L6.5 12.5L8.2 14H0V12H7.1ZM15 11V14H12V16L8 12.5L12 9V11H15Z" />
+  </base-svg>
+</template>
+
+<script>
+export default {
+  name: "WithdrawalIcon",
+};
+</script>
