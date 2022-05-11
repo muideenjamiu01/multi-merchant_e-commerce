b984f53f28845a25193a2e88b79c22bb334adb3e
chimailo <collins.chima.ilo@gmail.com>
2022-05-11 09:32:38 +0100

    admin table component

---
+++ components/Table/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="w-full overflow-x-auto">
+    <slot name="toolbar"></slot>
+    <table role="table" class="table w-full border-collapse my-4">
+      <thead class="table-head">
+        <slot name="tr-head"></slot>
+      </thead>
+      <tbody class="table-body">
+        <slot name="tr-body"></slot>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppTable",
+};
+</script>
+
+<style scoped>
+.table {
+  display: table;
+  border-spacing: 0;
+  min-width: 650px;
+}
+
+.table-head {
+  display: table-header-group;
+}
+
+.table-body {
+  display: table-row-group;
+}
+</style>
