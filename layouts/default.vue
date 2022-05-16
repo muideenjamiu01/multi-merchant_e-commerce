9de569b019a5fa499483be3083172d8525fba1b6
chimailo <collins.chima.ilo@gmail.com>
2022-05-16 11:12:39 +0100

    check offline in default layout and update pie-chart

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,23 @@
 <template>
   <div class="font-br-sonoma">
     <NavBar />
-      <nuxt class="min-h-screen" />
+      <div v-if="$nuxt.isOffline" class="max-w-sm mx-auto my-8">
+        <wifi-off-icon class="w-24 h-24 text-secondary-300"></wifi-off-icon>
+        <h1 class="text-xl text-secondary-500 font-medium">
+          You are currently offline
+        </h1>
+      </div>
+      <Nuxt class="min-h-screen" />
       <Footer />
   </div>
 </template>
+
+<script>
+import WifiOffIcon from "@/components/svg/WifiOff"
+
+export default {
+  components: {
+    WifiOffIcon
+  }
+}
+</script>
