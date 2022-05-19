2fa6bf33d8dc31e3486e5c19d50b9326d81733a1
chimailo <collins.chima.ilo@gmail.com>
2022-05-19 20:35:24 +0100

    update to modal component

--- components/merchant/withdrawals/transfers.vue
+++ components/merchant/withdrawals/transfers.vue
@@ -1,185 +1,5 @@
 <template>
   <div class="my-8 shadow">
-    <!-- Modal component -->
-    <div
-      v-if="transferModal"
-      class="
-        fixed
-        inset-0
-        bg-black/30
-        flex
-        justify-center
-        items-center
-        z-10
-        text-sm
-        shadow-lg
-      "
-      @click="closeTransferModal"
-    >
-      <div class="bg-white w-full max-w-md" @click.stop="">
-        <h1 class="p-4 border-b font-medium text-base">Make Transfer</h1>
-
-        <div
-          v-if="error"
-          class="
-            flex
-            relative
-            items-center
-            text-error-900
-            my-4
-            rounded
-            bg-error-50
-            w-full
-          "
-        >
-          <span class="text-center flex-grow p-2">{{ error }}</span>
-          <icon-button
-            @click="closeErrorMessage"
-            size="small"
-            class="hover:bg-error-100 rounded-full"
-          >
-            <cancel-icon></cancel-icon>
-          </icon-button>
-        </div>
-
-        <form class="p-4" @submit.prevent="handleTransfer">
-          <legend class="font-medium my-4">Transfer to</legend>
-          <div v-if="hasSavedAccountDetails" class="mb-8 flex items-center">
-            <span class="h-full w-1/2">
-              <input
-                id="savedAccount"
-                v-model="transferTo"
-                type="radio"
-                value="saved"
-                class="sr-only"
-                checked
-              />
-              <label
-                for="savedAccount"
-                class="
-                  border border-solid border-secondary-200
-                  flex
-                  items-center
-                  justify-center
-                  h-10
-                  bg-transparent
-                  px-[15px]
-                  py-[5px]
-                  text-sm
-                  outline-0
-                "
-                >Saved account</label
-              >
-            </span>
-            <span class="h-full w-1/2">
-              <input
-                id="newAccount"
-                v-model="transferTo"
-                type="radio"
-                value="another"
-                class="sr-only"
-              />
-              <label
-                for="newAccount"
-                class="
-                  border border-solid border-secondary-200
-                  flex
-                  items-center
-                  justify-center
-                  h-10
-                  bg-transparent
-                  px-[15px]
-                  py-[5px]
-                  text-sm
-                  outline-0
-                "
-                >Another account</label
-              >
-            </span>
-          </div>
-
-          <text-input
-            v-model="transferFields.amount"
-            type="number"
-            name="amount"
-            required
-          >
-            Amount to Transfer
-          </text-input>
-
-          <template v-if="transferTo === 'another'">
-            <text-input
-              v-model="transferFields.account_number"
-              type="text"
-              name="accNum"
-              required
-            >
-              Account Number
-            </text-input>
-          </template>
-
-          <div v-if="transferTo === 'another'" class="my-4">
-            <label for="bank-select" class="block mb-2 text-sm font-medium"
-              >Select your bank</label
-            >
-            <select
-              id="bank-select"
-              v-model.trim="transferFields.bank_name"
-              name="banks"
-              class="
-                flex
-                items-center
-                outline-0
-                border border-secondary-200
-                rounded-md
-                shadow-sm
-                focus:outline-none
-                focus:bg-primary-100
-                focus:border-primary-200
-                focus:ring-primary-200
-                focus:ring-1
-                sm:text-sm
-                p-2
-                my-1
-                w-full
-              "
-              required
-            >
-              <option value="" disabled>--Select a bank--</option>
-              <option v-for="bank in banks" :key="bank.code" :value="bank.name">
-                {{ bank.name }}
-              </option>
-            </select>
-          </div>
-
-          <div class="flex justify-end mt-8 gap-4">
-            <app-button
-              variant="outlined"
-              color="secondary"
-              @click="closeTransferModal"
-            >
-              Cancel
-            </app-button>
-            <app-button
-              variant="contained"
-              color="success"
-              :disabled="loading"
-              type="submit"
-              class="pl-4"
-            >
-              {{ loading ? "Transferring" : "Transfer" }}
-              <loading-spinners
-                v-if="loading"
-                size="small"
-                color="white"
-                class="mx-4"
-              ></loading-spinners>
-            </app-button>
-          </div>
-        </form>
-      </div>
-    </div>
-    <!-- End modal -->
     <app-table :columns="columns" :data="data">
       <template #toolbar>
         <table-toolbar :title="title">
@@ -216,16 +36,191 @@
         </table-row>
       </template>
     </app-table>
+
+    <transition name="fade">
+      <app-modal
+        v-if="transferModal"
+        :open="transferModal"
+        :close-modal="closeTransferModal"
+      >
+        <template #header>
+          <h2 class="font-medium text-xl">
+            Make Transfer
+          </h2>
+        </template>
+        <template #content>
+          <div
+            v-if="error"
+            class="
+              flex
+              relative
+              items-center
+              text-error-900
+              my-4
+              rounded
+              bg-error-50
+              w-full
+            "
+          >
+            <span class="text-center flex-grow p-2">{{ error }}</span>
+            <icon-button
+              @click="closeErrorMessage"
+              size="small"
+              class="hover:bg-error-100 rounded-full"
+            >
+              <cancel-icon></cancel-icon>
+            </icon-button>
+          </div>
+
+          <form class="min-w-[300px]" @submit.prevent="handleTransfer">
+            <legend class="font-medium my-4">Transfer to</legend>
+            <div v-if="hasSavedAccountDetails" class="mb-8 flex items-center">
+              <span class="h-full w-1/2">
+                <input
+                  id="savedAccount"
+                  v-model="transferTo"
+                  type="radio"
+                  value="saved"
+                  class="sr-only"
+                  checked
+                />
+                <label
+                  for="savedAccount"
+                  class="
+                    border border-solid border-secondary-200
+                    flex
+                    items-center
+                    justify-center
+                    h-10
+                    bg-transparent
+                    px-[15px]
+                    py-[5px]
+                    text-sm
+                    outline-0
+                  "
+                  >Saved account</label
+                >
+              </span>
+              <span class="h-full w-1/2">
+                <input
+                  id="newAccount"
+                  v-model="transferTo"
+                  type="radio"
+                  value="another"
+                  class="sr-only"
+                />
+                <label
+                  for="newAccount"
+                  class="
+                    border border-solid border-secondary-200
+                    flex
+                    items-center
+                    justify-center
+                    h-10
+                    bg-transparent
+                    px-[15px]
+                    py-[5px]
+                    text-sm
+                    outline-0
+                  "
+                  >Another account</label
+                >
+              </span>
+            </div>
+
+            <text-input
+              v-model="transferFields.amount"
+              type="number"
+              name="amount"
+              required
+            >
+              Amount to Transfer
+            </text-input>
+
+            <template v-if="transferTo === 'another'">
+              <text-input
+                v-model="transferFields.account_number"
+                type="text"
+                name="accNum"
+                required
+              >
+                Account Number
+              </text-input>
+            </template>
+
+            <div v-if="transferTo === 'another'" class="my-4">
+              <label for="bank-select" class="block mb-2 text-sm font-medium"
+                >Select your bank</label
+              >
+              <select
+                id="bank-select"
+                v-model.trim="transferFields.bank_name"
+                name="banks"
+                class="
+                  flex
+                  items-center
+                  outline-0
+                  border border-secondary-200
+                  rounded-md
+                  shadow-sm
+                  focus:outline-none
+                  focus:bg-primary-100
+                  focus:border-primary-200
+                  focus:ring-primary-200
+                  focus:ring-1
+                  sm:text-sm
+                  p-2
+                  my-1
+                  w-full
+                "
+                required
+              >
+                <option value="" disabled>--Select a bank--</option>
+                <option v-for="bank in banks" :key="bank.code" :value="bank.name">
+                  {{ bank.name }}
+                </option>
+              </select>
+            </div>
+          </form>
+        </template>
+        <template #footer>
+          <app-button
+            variant="outlined"
+            color="secondary"
+            @click="closeTransferModal"
+          >
+            Cancel
+          </app-button>
+          <app-button
+            variant="contained"
+            color="success"
+            :disabled="loading"
+            type="submit"
+            class="pl-4 ml-3"
+          >
+            {{ loading ? "Transferring" : "Transfer" }}
+            <loading-spinners
+              v-if="loading"
+              size="small"
+              color="white"
+              class="mx-4"
+            ></loading-spinners>
+          </app-button>
+        </template>
+      </app-modal>
+    </transition>
   </div>
 </template>
 
 <script>
 import AppTable from "@/components/Table";
+import AppModal from "@/components/Modals";
 import CancelIcon from "@/components/svg/Cancel.vue";
 
 export default {
   components: {
     AppTable,
+    AppModal,
     "cancel-icon": CancelIcon,
   },
   props: ["title", "columns", "data"],
@@ -365,5 +360,15 @@ export default {
 input:checked ~ label {
   border: 2px solid #ebf8fe;
   background-color: #ebf8fe;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 200ms ease-in;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
