03f6099664303b6ba05f65b6d6265094dc370bdc
Manos Akpujiha <manos.akpujiha@gmail.com>
2022-05-31 20:55:04 +0100

    single product page fixes

--- components/contents/ReviewSingleProduct.vue
+++ components/contents/ReviewSingleProduct.vue
@@ -1,32 +1,61 @@
 <template>
-    <p>
-        <!-- {{review}} -->
-    </p>
+  <div v-if="index <= 2" class="text-primary-black ml-0">
+    <div class="bg-primary-blue-light p-4 ml-0 mb-6">
+      <div>
+        <div class="mb-2 flex items-center my-[10px]">
+          <img class="rounded-full bg-primary-gray w-[40px] h-[40px] my-[10px]" :src="customerAvatar" :alt="customerName">
+          <h1 class="text-sm font-medium ml-2 my-[10px]">
+            {{customerName.toUpperCase()}}
+          </h1>
+          
+        </div>
+        <p class="text-sm flex my-[10px]">
+         <ContentsRating 
+            class="mr-[6px]"
+            :rating="rating"
+            :ratingLabel="`Customer Rating`"
+          />
+          
+        </p>
+        <p class="font-medium text-sm flex my-[10px]">Posted on: <span class="ml-2 font-light">{{handleDate}}</span></p>
+        <p class="font-medium text-sm my-[10px]">
+          Comment: <span class="font-light">{{comment[0].toUpperCase()}}{{comment.slice(1)}}.</span>
+        </p>
+       
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        data () {
-            return {
-                review : "review"
-            }
-        },
-        async getProductReview(id) {
-             try {
-                const response = await this.$axios.get(
-                    // `https://youstore-products.herokuapp.com/v1/product/${productId}/review`,
-                    `https://youstore-products.herokuapp.com/v1/product/${id}`,
-                   
-                );
-                alert("success")
-                console.log(response);
-            // commit("setReviews", reviewData);
-            } catch (error) {
-            // commit("setError", error.message);
-            alert("error")
-            } finally {
-            // commit("setLoading", false);
-            }
-        },
+
+export default {
+  data () {
+    return {
+      datePosted : "",
+      name: ''
     }
+  },
+  props: {
+    customerName: "",
+    createdAt: "",
+    rating: "",
+    comment:"",
+    customerAvatar:'',
+    id:'',
+    index:''
+  },
+  methods : {
+    
+  },
+  computed: {
+    handleDate() {
+      return `${this.datePosted.slice(8, 10)}/${this.datePosted.slice(5, 7)}/${this.datePosted.slice(0, 4)}`;
+    },
+  },
+  mounted () {
+    this.datePosted = this.$props.createdAt.split('T')[0];
+    this.name = this.$props.customerName.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
+  },
+}
 </script>
--- components/contents/SingleProduct.vue
+++ components/contents/SingleProduct.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="font-light mt-[32px] pl-[48px] pr-[40px] text-primary-black">
-    <div class="flex mt-[32px]">
+  <div class="flex flex-col font-light mt-5 pl-[48px] pr-[40px] text-primary-black">
+    <div class="flex flex-col md:flex-row mt-5">
       <main class="w-[70%]">
-        <div  class="flex ">
+        <div  class="flex flex-col md:flex-row">
           
-          <div class="w-full h-min max-w-lg min-w-[25%] border border-[#0B4B58]">
-            <img
+          <div class="w-full h-min max-w-lg min-w-[25%] border mt-5">
+            <img v-if="product === undefined" src="@/assets/images/icons/vehicle.svg" alt="">
+            <img v-else
               :src="getProductImage(product.images[0])"
               class="w-full h-[auto]"
               alt="product.name"
             />
           </div>
-          <div class="mx-[32px]  min-w-[25%]">
-            <h1 class="text-xl capitalize">{{ product.name }}</h1>
+          <div class="my-5 mx-[32px]  min-w-[25%]">
+            <h1 class="text-xl capitalize font-medium">Product Name: <span class="font-normal">{{ product.name }}</span></h1>
             <div class="flex items-center">
-              <Rating rating=""  ratingLabel="AVG. Rating"/>
-              <button @click="fetchSomething">fetch</button>
+              <Rating :rating='5'  ratingLabel="AVG. Rating"/>
               <p class="text-primary-blue-dark ml-2.5 text-sm">
               </p>
             </div>
@@ -31,9 +31,9 @@
               Add to cart
             </app-button>
             <div class="mt-[32px]">
-          <h1 class="text-xl mb-3">Product Description</h1>
+          <h1 class="text-xl mb-3 font-medium">Product Description</h1>
           <p>
-            {{ product.description }}
+            {{ `${product.description[0].toUpperCase()}${product.description.slice(1)} `  }}
           </p>
         </div>
         
@@ -43,8 +43,28 @@
      
       </main>
       <aside class="max-w-[20%]">
+        <div  class="min-w-88 bg-primary-blue-light py-2.5 pb-10 font-light mt-5">
+          <h1 class="font-medium px-6 py-4">Seller Information</h1>
+          <div class="border-t border-gray-200" />
+          <div class="px-6 py-4">
+            <div class="flex">
+              <img width="50px" height="50px" :src="merch.avatar" :alt="merch.storeName">
+              <h1 v-if="merch.storeName === undefined" class="ml-5 pt-5 text-primary-blue-dark">STORE</h1>
+              <h1 v-else class="ml-5 pt-5 text-primary-blue-dark"> {{getCaps(merch.storeName)}} STORE</h1>
+              
+            </div>
+            <p class="text-sm mt-5 font-medium">
+            Merchant E-mail: <span class="font-light">{{merch.email}}</span> 
+            </p>
+            <p class="text-sm mt-5 font-medium">
+            Phone No.: <span class="font-light">{{merch.phoneNo}}</span>
+            </p>
+            <p class="mt-5 text-sm font-medium">Customer Rating: <span class="font-light">{{merch.customerRating}}</span></p>
+            <p class="mt-5 text-sm font-medium">Order Fulfilment Rate: <span class="font-light">{{merch.orderFufillmentRate}}</span></p>
+          </div>
+        </div>
         <div
-          class="min-w-88 bg-primary-blue-light px-7 pb-10 font-light xl:mt-0"
+          class="hidden md:block mt-5 min-w-88 bg-primary-blue-light px-7 pb-10 font-light xl:mt-0"
         >
           <h1 class="font-medium">Delivery and Shipping</h1>
           <p class="tex-xs mt-10">
@@ -108,20 +128,7 @@
             </div>
           </div>
         </div>
-        <div class="min-w-88 bg-primary-blue-light py-2.5 pb-10 font-light mt-8">
-        <h1 class="font-medium px-6 py-4">Seller Information</h1>
-        <div class="border-t border-gray-200" />
-        <div class="px-6 py-4">
-          <h1 class="text-primary-blue-dark">Mummy's choices store</h1>
-          <p class="text-sm mt-5">
-            Dealers in the best aenean sodales mattis rutrum. Duis id
-            consectetur neque, eu ullam corper lorem. Aenean a nulla ullam
-            corper, congue ligula ut, gravida lacus.
-          </p>
-          <p class="mt-4 text-sm">Customer Rating: Good</p>
-          <p class="mt-2 text-sm">Order Fulfilment Rate: Excellent</p>
-        </div>
-        </div>
+        
       </aside>
     </div>
 
@@ -129,8 +136,17 @@
       
     </div>
     <div class="mt-8">
-      <h1 class="font-bold">49 Comments from verified purchase</h1>
-      <!-- <ContentsReviews v-for="n in 3" :key="n" /> -->
+      <h1 class="font-bold">{{product.reviews.length}} Comments from verified purchase</h1>
+      <ContentsReviewSingleProduct v-for="(review, index) in product.reviews" :key="review._id"
+        :customerName="review.customerName"
+        :createdAt= "review.createdAt"
+        :rating ="review.rating"
+        :comment="review.comment"
+        :customerAvatar='review.customerAvatar'
+        :id='review._id'
+        :index="index"
+      
+      />
       <NuxtLink
         :to="$route.params.product + '/reviews'"
         class="font-light text-primary-blue-dark mt-6 flex justify-end"
@@ -148,14 +164,22 @@ import { mapGetters, mapActions } from "vuex";
 import Rating from "./Rating";
 
 export default {
+  data() {
+        return {
+             merch: [],
+        }
+    },
     async mounted() {
-        console.log(this.product);
         this.getOneMerchant()
+        console.log(this.product)
     },
     methods: {
+      getCaps(data) {
+       return this.merch.storeName.toUpperCase()
+      },
        async getOneMerchant() {
-          const response = await this.$axios.$get('https://api-2445583927843.production.gw.apicast.io:443/api/users/v1/merchants/one/623c4fc948190e352f309664')
-          console.log('GET_ONE_MERCHANT', response.data)
+          const response = await this.$axios.$get(`https://api-2445583927843.production.gw.apicast.io:443/api/users/v1/merchants/one/${this.product.merchantId}`)
+          this.merch = response.data;
         },
         ...mapActions("cart", ["addProductToCart"]),
         ...mapActions("wishlist", ["addToWishlist"]),
